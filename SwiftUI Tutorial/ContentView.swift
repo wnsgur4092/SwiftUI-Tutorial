981fc5b44d4e4130f72d4ba171f897b4f906da88
Jun <wnsgur4092@gmail.com>
2022-09-02 01:29:31 +1000

    Initial Commit

---
+++ SwiftUI Tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Tutorial
+//
+//  Created by JunHyuk Lim on 2/9/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Tutorial/SwiftUI_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TutorialApp.swift
+//  SwiftUI Tutorial
+//
+//  Created by JunHyuk Lim on 2/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
